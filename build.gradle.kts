0d91cb0fde7ebbcf4de087138b5fd85ba27d2eb8
lontten <lontten@163.com>
2024-11-01 00:44:51 +0800

    🔖 发布2.117.9.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.29.0"
 }
 group = "io.github.lontten"
-version = "2.117.8.RELEASE"
+version = "2.117.9.RELEASE"
 
 java {
     toolchain {
